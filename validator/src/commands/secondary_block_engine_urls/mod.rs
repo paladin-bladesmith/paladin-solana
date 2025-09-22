7eb6d3f8f54fb9f754a59b1e92e4f1f062035e12
Art3mix <r3mix.il@gmail.com>
2025-09-22 10:10:06 +0300

    Add runtime MBE configuration

---
+++ validator/src/commands/secondary_block_engine_urls/mod.rs
@@ -0,0 +1,34 @@
+use {
+    crate::{admin_rpc_service, cli::DefaultArgs, commands::Result},
+    clap::{App, Arg, ArgMatches, SubCommand},
+    std::path::Path,
+};
+
+pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
+    SubCommand::with_name("set-secondary_block_engine_urls")
+        .about("Set secondary block engine urls")
+        .arg(
+            Arg::with_name("urls")
+                .long("urls")
+                .help("Secondary block engine urls. Set to empty string to remove all.")
+                .takes_value(true)
+                .multiple(true)
+                .required(true),
+        )
+}
+
+pub fn execute(subcommand_matches: &ArgMatches, ledger_path: &Path) -> Result<()> {
+    let secondary_block_engine_urls = subcommand_matches
+        .values_of("secondary_block_engines_urls")
+        .unwrap_or_default()
+        .map(ToString::to_string)
+        .collect();
+    let admin_client = admin_rpc_service::connect(ledger_path);
+    admin_rpc_service::runtime().block_on(async move {
+        admin_client
+            .await?
+            .set_secondary_block_engine_urls(secondary_block_engine_urls)
+            .await
+    })?;
+    Ok(())
+}
